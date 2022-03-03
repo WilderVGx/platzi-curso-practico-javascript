65e176c475efd2b97ab999be8b341ed4a2143f40
WilderVGx <wvasquezgx@gmail.com>
2022-03-02 22:12:21 -0500

    main

--- figuras.js
+++ figuras.js
@@ -1,3 +1,4 @@
+// Código del cuadrado
 const ladoCuadrado = 5;
 console.log("Los lados del cuadrado miden: " + ladoCuadrado + "cm");
 
@@ -6,3 +7,9 @@ console.log("El perímetro del cuadrado es: " + perimetroCuadrado + "cm");
 
 const areaCuadrado = ladoCuadrado * ladoCuadrado;
 console.log("El área del cuadrado es: " + areaCuadrado + "cm²");
+
+// Código del triángulo
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+console.log("Los lados del triángulo miden: " + ladoTriangulo1 + "cm, " + ladoTriangulo2 + "cm, " + baseTriangulo + "cm");
