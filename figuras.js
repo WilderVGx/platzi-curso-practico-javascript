a4d10df4e673872b581232cf37daa45dc7a37f26
WilderVGx <wvasquezgx@gmail.com>
2022-03-02 22:03:44 -0500

    main

--- figuras.js
+++ figuras.js
@@ -1,6 +1,8 @@
 const ladoCuadrado = 5;
-console.log("Los lados del cuadrado miden: " + ladoCuadrado);
+console.log("Los lados del cuadrado miden: " + ladoCuadrado + "cm");
 
 const perimetroCuadrado = ladoCuadrado * 4;
-console.log("El perímetro cuadrado es: " + perimetroCuadrado);
+console.log("El perímetro del cuadrado es: " + perimetroCuadrado + "cm");
 
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("El área del cuadrado es: " + areaCuadrado + "cm²");
